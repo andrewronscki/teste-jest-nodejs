f2d4209891361474da588e9e366a01482f9d843d
André Wronscki <andrewronscki@gmail.com>
2019-08-23 19:29:19 -0300

    primeiro commit

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+    jest: true,
+  },
+  extends: ['airbnb-base', 'prettier'],
+  plugins: ['prettier'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+    'prettier/prettier': 'error',
+    'class-methods-use-this': 'off',
+    'no-param-reassign': 'off',
+    cammelcase: 'off',
+    'no-unused-vars': ['error', { argsIgnorePattern: 'next' }],
+    semi: 'off',
+    'import/prefer-default-export': 'off',
+  },
+}
